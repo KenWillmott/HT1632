504c808674541b5d4167b8990313a5006cb8062f
KenWillmott <ken@kenwillmott.com>
2016-02-28 19:44:17 +0800

    add beerware license

--- HT1632.cpp
+++ HT1632.cpp
@@ -10,6 +10,25 @@
   32x8 red LED matrix
   by Ken Willmott, February 17, 2016
 
+/* code between:
+
+// ********************************
+// JY-MCU modifications
+
+and
+
+// end of JY-MCU mods
+//***********************************
+
+is subject to the following license:
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <phk@FreeBSD.ORG> wrote this file.  As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return.   Poul-Henning Kamp
+ * ----------------------------------------------------------------------------
+
+
 */
 
 // Hardware:
